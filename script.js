2c5ad21c80671003179cd323c7b6dc90198afdd3
eugborisov <eug.borisov91@gmail.com>
2019-11-04 19:06:09 +0300

    increased scroll disabling timeout

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ if ($('#carousel').is(':visible')) {
          $(this).carousel('next');
        }
      $(this).addClass('disabled');
-     setTimeout(function() {$("#carousel").removeClass('disabled')}, 1500);
+     setTimeout(function() {$("#carousel").removeClass('disabled')}, 2000);
      } else {
        console.log('disabled scroll');
      }
